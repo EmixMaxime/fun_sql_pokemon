8eab37ff21051d6a04a9bebd7e8061a11e8ebaeb
EmixMaxime <emixmaxime@outlook.fr>
2017-10-15 11:04:55 +0200

    (improve) use cursor instead of multiple requests

--- maj_efficacite_func.sql
+++ maj_efficacite_func.sql
@@ -8,39 +8,42 @@ AS $$
     selected_type_id1 type.id%TYPE;
     selected_type_id2 type.id%TYPE;
 
-    -- d integer; -- get diagnostic
-
-  BEGIN
-
-    -- On récupère les ID en fonction du nom
-    SELECT id INTO selected_type_id1
-      FROM type
+    typeCursor CURSOR FOR
+      SELECT id FROM type
       WHERE lower(nom) = lower(p_type_nom1)
-    ; 
-    -- lower -> upper serait plus judicieux, le `nom` étant déjà en uppercase.
-
-    -- Si l'user me passe un nom de type qui n'existe pas, j'arrête !
-    IF NOT FOUND THEN
-      RAISE EXCEPTION 'Type % not found.', p_type_nom1;
-    END IF;
-
-    SELECT id INTO selected_type_id2
-      FROM type
-      WHERE lower(nom) = lower(p_type_nom2)
+      OR lower(nom) = lower(p_type_nom2)
     ;
 
-    IF NOT FOUND THEN
-      RAISE EXCEPTION 'Type % not found.', p_type_nom2;
-    END IF;
+    -- typeCursor refcursor;
+  BEGIN
+
+    p_type_nom1 := lower(p_type_nom1);
+    p_type_nom2 := lower(p_type_nom2);
+
+    -- Récupération et vérifications des `type`s.
+    OPEN typeCursor;
+
+      FETCH typeCursor INTO selected_type_id1;
+      IF NOT FOUND THEN
+        RAISE EXCEPTION 'Type % not found.', p_type_nom1;
+      END IF;
+
+      IF p_type_nom1 = p_type_nom2 THEN
+        selected_type_id2 := selected_type_id1;
+      ELSE
+        FETCH typeCursor INTO selected_type_id2;
+        IF NOT FOUND THEN
+          RAISE EXCEPTION 'Type % not found.', p_type_nom1;
+        END IF;
+      END IF;
+
+    CLOSE typeCursor;
 
     -- Tout est ok, je peux update
     UPDATE efficacite SET taux = p_eff
       WHERE type_id_1 = selected_type_id1
       AND type_id_2 = selected_type_id2
     ;
-
-    -- GET DIAGNOSTICS d = ROW_COUNT;
-    -- RAISE NOTICE '% tuples modifiés', d;
 
   END;
 
