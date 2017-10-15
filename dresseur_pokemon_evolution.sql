569348e88e519f983ae1a84dcb84ad8f38649ee9
EmixMaxime <emixmaxime@outlook.fr>
2017-10-15 10:22:18 +0200

    (add) dresseur_pokemon_evolution function

---
+++ dresseur_pokemon_evolution.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE FUNCTION dresseur_pokemon_evolution(dresseur_pok_id integer)
+RETURNS void
+AS $$
+
+DECLARE
+  d integer; -- get diagnostic
+BEGIN
+  UPDATE dresseur_pokemon
+    SET points_evolution = points_evolution +1
+    WHERE id = dresseur_pok_id
+  ;
+
+  GET DIAGNOSTICS d = ROW_COUNT;
+
+  -- Check if an update has been performed
+  IF d < 1 THEN
+    RAISE EXCEPTION 'dresseur_pokemon id = % not found', dresseur_pok_id;
+  END IF;
+
+END;
+$$ language 'plpgsql';
