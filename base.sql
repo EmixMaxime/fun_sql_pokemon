c9b11039c610de49083a9eba85bd132dc565f430
Erwann Fiolet <erwann62100@gmail.com>
2017-10-22 16:39:28 +0200

    add view dresseur_pokemon_info

--- base.sql
+++ base.sql
@@ -867,3 +867,16 @@ AS $$
   END;
 
 $$ language 'plpgsql';
+
+
+CREATE OR REPLACE VIEW dresseur_pokemon_info AS
+  SELECT
+    p.nom AS pokemon_nom, p.id AS pokemon_id,
+    d.id AS dresseur_id, d.pseudo, d.nom AS dresseur_nom, d.prenom
+  FROM dresseur_pokemon AS dp
+  INNER JOIN pokemon AS p
+    ON p.id = dp.pokemon_id
+  INNER JOIN dresseur AS d
+    ON d.id = dp.dresseur_id
+  ORDER BY dresseur_id
+;
