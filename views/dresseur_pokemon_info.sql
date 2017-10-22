447ce07d5b1c7346f754e6bf6066ac02fee9c261
EmixMaxime <emixmaxime@outlook.fr>
2017-10-22 11:09:51 +0200

    (improve) dresseur_pokemon_info

--- views/dresseur_pokemon_info.sql
+++ views/dresseur_pokemon_info.sql
@@ -1,5 +1,7 @@
 CREATE OR REPLACE VIEW dresseur_pokemon_info AS
-  SELECT p.nom, p.id AS pokemon_id, d.pseudo, d.id AS dresseur_id
+  SELECT
+    p.nom AS pokemon_nom, p.id AS pokemon_id,
+    d.id AS dresseur_id, d.pseudo, d.nom AS dresseur_nom, d.prenom
   FROM dresseur_pokemon AS dp
   INNER JOIN pokemon AS p
     ON p.id = dp.pokemon_id
