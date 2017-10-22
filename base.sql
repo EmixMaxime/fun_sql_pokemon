f292e3882ad69f0cc40acafe197d2dc9d80a977e
Erwann Fiolet <erwann62100@gmail.com>
2017-10-22 21:13:24 +0200

    fix

--- base.sql
+++ base.sql
@@ -882,3 +882,21 @@ BEGIN
 END;
 $$ language 'plpgsql';
 
+CREATE OR REPLACE FUNCTION affichage_combat(numero int, eff float, vie int) RETURNS void
+AS $$
+
+BEGIN
+  RAISE NOTICE 'Le pokemon % attaque !',numero;
+      IF eff > 1 THEN
+        RAISE NOTICE 'C est super efficace';
+      ELSE 
+        IF eff < 1 THEN
+          RAISE NOTICE 'Ce n est pas tres efficace';
+        END IF;
+      END IF;
+      RAISE NOTICE 'Il reste % PV au pokemon %',vie,numero;
+      
+END;
+
+$$ language 'plpgsql';
+  
